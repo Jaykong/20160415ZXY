5fb633a9e888abe7e9bb2a3a5fce51b207778c1e
silin chen <499801661@qq.com>
2016-04-17 21:42:50 +0800

    lianxi

---
+++ exercise/chp2/练习/2.5练习1.5/2.5练习1.5/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  2.5练习1.5
+//
+//  Created by silin chen on 16/4/17.
+//  Copyright © 2016年 silin chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        int answer,result;
+        answer = 100;
+        result = answer - 10;
+        NSLog(@"The result is %i",result + 5);
+    }
+    return 0;
+}
