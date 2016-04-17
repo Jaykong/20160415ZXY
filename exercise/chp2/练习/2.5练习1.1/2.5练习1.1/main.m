3c77a5e9f304c16483e7ccf727ff552524a138cc
silin chen <499801661@qq.com>
2016-04-17 16:19:35 +0800

    lianxi

---
+++ exercise/chp2/练习/2.5练习1.1/2.5练习1.1/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  2.5练习1.1
+//
+//  Created by silin chen on 16/4/17.
+//  Copyright © 2016年 silin chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"In Objective-C, lowercase letters are significant.");
+        NSLog(@"main is where program execution begins.");
+        NSLog(@"Open and closed braces enclose program statements in a routine.");
+        NSLog(@"All program statements must be terminated by a semicolon.");
+    
+    }
+    return 0;
+}
---
+++ exercise/chp2/练习/2.5练习1.2/2.5练习1.2/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  2.5练习1.2
+//
+//  Created by silin chen on 16/4/17.
+//  Copyright © 2016年 silin chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        int i;
+        i = 1;
+        NSLog(@"Testing...");
+        NSLog(@"....%i",1);
+        NSLog(@"..)
+    }
+    return 0;
+}
